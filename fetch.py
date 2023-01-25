c7fc06c78290f9af6eac471945b696d65ca944c3
Jack Burkhardt <mail@jackburkhardt.com>
2023-01-25 13:13:17 -0600

    remove combiner, just play as carousel

--- fetch.py
+++ fetch.py
@@ -2,7 +2,7 @@ from pytube import YouTube
 import os
 import sys
 from canvasapi import Canvas
-import combine
+import json
 import threading
 import re
 from dotenv import load_dotenv
@@ -22,7 +22,7 @@ total_videos = 0
 download_queue = []
 video_path = ""
 
-student_names_ids = {}
+ids_to_names = {}
 
 def init(id):
     global assignment
@@ -30,14 +30,14 @@ def init(id):
     assignment = course.get_assignment(id)
     video_path = f"{id}"
 
-    sys.stdout.write(f"Fetching submissions for assignment {assignment.name}...\n")
+    sys.stdout.write(f"Fetching submissions for {assignment.name}...\n")
     download_submissions()
 
 def download_submissions():
     global submissions
     submissions = [assignment.get_submission(229733)]
     try:
-        print(assignment.name)
+        #print(assignment.name)
         os.mkdir(f"{video_path}")
     except FileExistsError:
         pass
@@ -48,33 +48,34 @@ def download_submissions():
     for submission in submissions:
         # if already graded, skip
         if submission.score is not None: continue
-        #print(submission.user_id)
 
         # if submission is url and url is youtube, download video asynchronously
         if submission.submission_type == "online_text_entry" and ("youtube" in submission.body or "youtu.be" in submission.body):
-            student_names_ids[submission.user_id] = "Test Student"
+            ids_to_names[submission.user_id] = "Test Student"
             #canvas.get_user(submission.user_id).name
             threading.Thread(target=download_submission, args=(submission,)).start()
             global total_videos
             total_videos += 1
         else:
-            sys.stderr.write(f"Submission made by {student_names_ids[submission.user_id]} does not include a youtube link! Skipping...")
+            sys.stderr.write(f"Submission made by {ids_to_names[submission.user_id]} does not include a youtube link! Skipping...")
             return
 
 
 def download_submission(submission):
     # look for youtube.com or youtu.be in the submission text, and then grab the whole url
-    if "youtube.com" in submission.body:
+    if "youtube.com/shorts" in submission.body:
+        url = re.search("https://www.youtube.com/shorts/[a-zA-Z0-9_-]+", submission.body).group(0)
+    elif "youtube.com" in submission.body:
         url = re.search("https://www.youtube.com/watch\?v=[a-zA-Z0-9_-]+", submission.body).group(0)
     elif "youtu.be" in submission.body:
-        url = re.search("https://youtu.be/[a-zA-Z0-9_-]+", submission.text).group(0)
+        url = re.search("https://youtu.be/[a-zA-Z0-9_-]+", submission.body).group(0)
 
     # download video to current directory/assignment name
     try:
         video = YouTube(url, on_complete_callback=lambda x, y: show_progress(video))
         download_queue.append(video)
     except:
-        sys.stderr.write(f"Error downloading video for submission made by {student_names_ids[submission.user_id]}! Skipping...")
+        sys.stderr.write(f"Error downloading video for submission made by {ids_to_names[submission.user_id]}! Skipping...")
         return
 
     stream = video.streams.filter(progressive=True, file_extension="mp4").last()
@@ -89,15 +90,17 @@ def show_progress(video):
 
     # if all videos have been downloaded, start the combine.py script
     if len(download_queue) == 0:
-        start_combine()
+        with open(f"{video_path}/ids_to_names.json", "x") as f:
+            f.write(json.dumps(ids_to_names))
+        sys.stdout.write("All videos downloaded! Run 'py play.py <assignment_id>' to play the videos and start grading.\n")
 
-def start_combine():
+#def start_combine():
     # start the combine.py script
-    combine.combine(video_path, student_names_ids)
+#    combine.combine(video_path, student_names_ids)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        sys.stderr.write("Usage: python3 download.py <assignment_id>")
+        sys.stderr.write("Error: Invalid number of arguments! Usage: py download.py <assignment_id>")
         sys.exit(1)
     init(sys.argv[1])
 
--- play.py
+++ play.py
@@ -2,77 +2,107 @@ import vlc
 import sys
 import os
 import json
-import time
 from dotenv import load_dotenv
 from canvasapi import Canvas
 
 vlc_player = None
-names_and_times = None
 assignment_id = None
 
-grades = {}
+videos = []
+curr = 0
 
-def play():
-    # play video
+grades = {}
+ids_to_names = {}
+
+def play(id):
     global vlc_player
+
+    # if id is None, we are not playing another video. keep playing!
+    if id == None:
+        cmd = input("Enter command: \n")
+        do_command(cmd, videos[curr])
+
+    # create vlc player if it doesn't exist
+    if vlc_player is None:
+        vlc_player = vlc.MediaPlayer()
+
+    # play video at path id
+    vlc_player.set_mrl(f"{os.getcwd()}/{path}/{id}.mp4")
     vlc_player.play()
 
-    sys.stdout.write("Video playing. Press 'n' to skip to the next student, 'p' to skip to the previous student, 'g <grade>' to set the grade, or 'exit' to exit.\n")
+    sys.stdout.write(f"Currently viewing: {ids_to_names[id]}.\nPress 'n' to skip to the next student, 'p' to skip to the previous student, 'g <grade>' to set the grade, or 'exit' to exit.\n")
+    cmd = input("Enter command: \n")
+    do_command(cmd, id)
+   
 
-    # every second, check if the video is open 
-    while vlc_player:
-        # if it is, get the current time
-        current_time = vlc_player.get_time()
-
-        # loop through names_and_times and check between each time
-        for name, times in names_and_times.items():
-            # if the current time is between the start and end time, print the name
-            if current_time >= times[0] and current_time <= times[1]:
-                sys.stdout.write(f"Currently viewing: {name}\n")
-                # accept input for the grade
-                grade = input("Enter command: \n")
-                # command for setting grade: g <grade>
-                if grade.startswith("g ") and len(grade.split(" ")) == 2:
-                    grades[name] = grade.split(" ")[1]
-                    sys.stdout.write(f"Set grade for {name} to {grade.split(' ')[1]}.\n")
-                # command for skipping to next student: n
-                elif grade == "n":
-                    sys.stdout.write("Skipping to next student...\n")
-                    vlc_player.set_time(int(times[1]))
-                    break
-                # command for skipping to previous student: p
-                elif grade == "p":
-                    sys.stdout.write("Skipping to previous student...\n")
-                    vlc_player.set_time(int(times[0]))
-                    break
-                # command for quitting: exit
-                elif grade == "exit":
-                    sys.stdout.write("Exiting playback...\n")
-                    vlc_player.stop()
-                    final_prompt()
-            
-
-             # sleep for 1 second
-        time.sleep(1)
-
-    # when the video is done playing, submit the grades
-    final_prompt()
-
+def do_command(cmd, id):
+    global curr
+    
+    if cmd.startswith("g ") and len(cmd.split(" ")) == 2:
+        name = ids_to_names[id]
+        grade = cmd.split(" ")[1]
+        
+        # check if grade is float or int
+        try:
+            float(grade)
+        except ValueError:
+            sys.stdout.write("Error: Invalid grade! Please enter a number.\n")
+            play(None)
+        grades[id] = grade
+        sys.stdout.write(f"Set grade for {name} to {grades[id]}.\n")
+        
+        # skip to next student
+        if curr + 1 >= len(videos):
+            vlc_player.stop()
+            sys.stdout.write("Finished playing the last submission.\n")
+            final_prompt()
+        else:
+            curr += 1
+            play(videos[curr])
+    
+    # command for skipping to next student: n
+    elif cmd == "n":
+        sys.stdout.write("Skipping to next student...\n")
+        if curr + 1 >= len(videos):
+            sys.stdout.write("Already playing the last submission!\n")
+            play(None)
+        curr += 1
+        play(videos[curr])
+    
+    # command for skipping to previous student: p
+    elif cmd == "p":
+        sys.stdout.write("Skipping to previous student...\n")
+        if curr - 1 < 0:
+            sys.stdout.write("Already playing the first submission!\n")
+            play(None)
+        curr -= 1
+        play(videos[curr])
+    
+    # command for quitting: exit
+    elif cmd == "exit":
+        sys.stdout.write("Exiting playback...\n")
+        vlc_player.stop()
+        final_prompt()
 
 def final_prompt():
     i = input("Type 'list' to see the grades, 'submit <assignment id>' to submit them to Canvas, or 'exit' to exit.\n")
 
     if i == "list":
-        for name, grade in grades.items():
-            sys.stdout.write(f"{name}: {grade}\n")
+        for id, grade in grades.items():
+            sys.stdout.write(f"{ids_to_names[id]}: {grade}\n")
         final_prompt()
     elif i.startswith("submit") and len(i.split(" ")) == 2:
         submit(int(i.split(" ")[1]))
     elif i == "exit":
+        sys.stdout.write("Exiting...\n")
+        with open(f"{path}/grades.json", "w") as f:
+            f.write(json.dumps(grades))
         sys.exit(0)
 
 
 def submit(id):
+
+    sys.stdout.write("Submitting grades to Canvas...\n")
     
     load_dotenv()
     canvas = Canvas(os.getenv("CANVAS_API_URL"), os.getenv("CANVAS_API_KEY"))
@@ -80,30 +110,36 @@ def submit(id):
     assignment = course.get_assignment(id)
 
     # loop through the grades and submit them
-    for name, grade in grades.items():
-        # get the user id from the name
-        user_id = names_and_times[name][2]
-        # get the submission
-        submission = assignment.get_submission(user_id)
-
-        # submit the grade
+    for id, grade in grades.items():
+        submission = assignment.get_submission(id)
         submission.edit(submission={"posted_grade": grade})
 
+    sys.stdout.write("Successfully submitted grades!\n")
+    sys.exit(0)
+
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.stderr.write("Error: Invalid number of arguments! Usage: py play.py <assignment id>")
+        sys.exit(1)
 
     # get video path from command line
     path = str(sys.argv[1])
 
-    # check if video exists
+    # check if videos exist
     if not os.path.exists(path):
-        sys.stderr.write("Error: Video not found at the given path!")
+        sys.stderr.write("Error: Videos not found at the given path!")
         sys.exit(1)
 
-    # create vlc player
-    vlc_player = vlc.MediaPlayer(f"{os.getcwd()}/{path}/COMBINED.mp4")
+    # list the videos within the directory without the extension
+    videos = [f.split(".")[0] for f in os.listdir(path) if f.endswith(".mp4")]
 
     # open the names and times txt file, deserialize it, and set the global variable
-    with open(f"{path}/names_and_times.json", "r") as f:
-        names_and_times = json.loads(f.read())
+    with open(f"{path}/ids_to_names.json", "r") as f:
+        ids_to_names = json.loads(f.read())
 
-    play()
+    # if there are grades already, load them
+    if os.path.exists(f"{path}/grades.json"):
+        with open(f"{path}/grades.json", "r") as f:
+            grades = json.loads(f.read())
+
+    play(videos[curr])
